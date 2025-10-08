9e6bbbc3a9e44f304bfa0694ea9bd26f11deaf59
Mustaqeem <Mustaqeem.A.Chowdhury@gmail.com>
2025-10-08 20:19:11 +0100

    Added the add item button, and some minor design and structure changes.

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
 import { menuArray } from '/data.js';
 
-const orderDetails = document.getElementById("order-details");
-let html = ``;
-menuArray.forEach(function(item) {
-    console.log(item.ingredients);
+const itemDetails = document.getElementById("items-list");
+const addItem = document.getElementById(`items-list`);
 
-    html += `
-    <div class="flex item-line">
-        <p class="item-emoji">${item.emoji}</p>
-        <div class="wrapper">
-            <p class="item-name">${item.name}</p>
-            <p class="item-ingredients">${item.ingredients.join(', ')}</p>
-            <p class="item-price">$${item.price}</p>
+addItem.addEventListener("click", function(event) {
+    addToBasket(event.target.id);
+})
+
+function addToBasket(id) {
+    console.log(id);
+
+    let html = `
+        <div>
+            <h2 class="order-title" id="order-title"></h2>
+            <section class="items-selected" id="items-selected"></section>
+            <p class="total-price" id="total-price">Total price:
+                <span>$${total-price}</span>
+            </p>
         </div>
-    </div>
+    `
+}
 
-`
-orderDetails.innerHTML = html;
-});
+function render() {
+    let html = ``;
+    menuArray.forEach(function(item) {
+        html += `
+        <div class="flex">
+            <p class="item-emoji">${item.emoji}</p>
+            <div class="wrapper">
+                <p class="item-name">${item.name}</p>
+                <p class="item-ingredients">${item.ingredients.join(', ')}</p>
+                <p class="item-price">$${item.price}</p>
+            </div>
+            <i class="fa-solid fa-plus" id="add-${item.name}"></i>
+        </div>
+        <div class="items-border"></div>
+    `
+
+    itemDetails.innerHTML = html;
+    });
+}
+
+render();
